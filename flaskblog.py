a5d4323011c6df29585ab6f9d910a3a39035d491
Jay-68 <kelvinngari1995@gmail.com>
2019-10-26 17:27:03 +0300

    Add templates folder and add navbar

--- flaskblog.py
+++ flaskblog.py
@@ -2,15 +2,31 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+posts = [
+    {
+        'author': 'James Ngari',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'October 24,2019'
+    },
+    {
+        'author': 'Brian Doe',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'October 22,2011'
+    }
+]
+
 
 @app.route('/')
 @app.route('/home')
 def hello():
-    return render_template('home.html')
+    return render_template('home.html',posts=posts)
+
 
 @app.route('/about')
 def about():
-    return render_template('about.html')
+    return render_template('about.html',title='About')
 
 
 if __name__ == '__main__':
