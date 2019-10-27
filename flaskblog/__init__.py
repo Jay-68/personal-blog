ce23b58d8cb0669a9975569d79e3a5ec483115c6
Jay-68 <kelvinngari1995@gmail.com>
2019-10-27 15:08:31 +0300

    reinstall virtualenv and bcrytp

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -1,11 +1,11 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from flask_bcrypt import Bcrypt
+# from flask_bcrypt import Bcrypt
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '5ea701ec97abbc6c97946e6d96a42cf0'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
-bcrypt = Bcrypt(app)
+# bcrypt = Bcrypt(app)
 
 from flaskblog import routes
