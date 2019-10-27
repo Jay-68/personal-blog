42f318a31fa4114e79dd7a068105ad0aae622011
Jay-68 <kelvinngari1995@gmail.com>
2019-10-27 21:19:58 +0300

    Add a function for saving profile pictures

--- flaskblog/routes.py
+++ flaskblog/routes.py
@@ -1,3 +1,5 @@
+import os
+import secrets
 from flask import render_template, url_for, flash, redirect,request
 from flaskblog import app,db
 from werkzeug.security import generate_password_hash,check_password_hash
@@ -71,11 +73,23 @@ def logout():
     logout_user()
     return redirect(url_for('home'))
 
+
+def save_picture(form_picture):
+    random_hex = secrets.token_hex(8)
+    _,f_ext = os.path.splitext(form_picture.filename)
+    picture_fn = random_hex + f_ext
+    picture_path = os.path.join(app.root_path, 'static/profile_pics/', picture_fn)
+    form_picture.save(picture_path)
+    return picture_fn
+
+
 @app.route('/account',methods=['GET','POST'])
 @login_required
 def account():
     form = UpdateAccountForm()
     if form.validate_on_submit():
+        if form.picture.data:
+
         current_user.username = form.username.data
         current_user.email = form.email.data
         db.session.commit()
